cccf6e086ed0be297c6c6e10caa1e106439c992e
hmdalm <hmd.alm143@gmail.com>
2023-05-28 20:36:00 +0530

    first commite

---
+++ src/main/java/com/college/CollegeMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.college;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMgmtApplication.class, args);
+	}
+
+}
